3f7a7baa997237da33f172059fc6c012ae515bd8
Andrés Maneiro <andres.maneiro@gmail.com>
2016-03-01 15:44:42 +0100

    DROP temporal table after use

--- scripts/subir_datos.sh
+++ scripts/subir_datos.sh
@@ -1,2 +1,3 @@
 shp2pgsql -d -s 32737 -W "cp1250" 160224_UtentesPRUEBA.shp public.entrada  | psql -h localhost -U postgres -d sixhiara
 psql -h localhost -U postgres -d sixhiara -f subir_datos.sql
+psql -h localhost -U postgres -d sixhiara -c "DROP TABLE public.entrada;"
