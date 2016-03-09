54a32162765fe0b884254fbdd69c64ca7b011b2a
Andrés Maneiro <amaneiro@icarto.es>
2016-03-09 11:51:43 +0100

    update domain values for boolean selects

--- deploy/populate_domains_table.sql
+++ deploy/populate_domains_table.sql
@@ -31,8 +31,8 @@ INSERT INTO domains.bacia (category, key, value, ordering, parent, tooltip) VALU
 INSERT INTO domains.bacia (category, key, value, ordering, parent, tooltip) VALUES ('bacia', 'Rovuma', NULL, NULL, NULL, NULL);
 
 INSERT INTO domains.contador (category, key, value, ordering, parent, tooltip) VALUES ('contador', NULL, NULL, '0', NULL, NULL);
-INSERT INTO domains.contador (category, key, value, ordering, parent, tooltip) VALUES ('contador', 'Existe', NULL, NULL, NULL, NULL);
-INSERT INTO domains.contador (category, key, value, ordering, parent, tooltip) VALUES ('contador', 'Não existe', NULL, NULL, NULL, NULL);
+INSERT INTO domains.contador (category, key, value, ordering, parent, tooltip) VALUES ('contador', 'Existe', 'true', NULL, NULL, NULL);
+INSERT INTO domains.contador (category, key, value, ordering, parent, tooltip) VALUES ('contador', 'Não existe', 'false', NULL, NULL, NULL);
 
 INSERT INTO domains.cultivo_tipo (category, key, value, ordering, parent, tooltip) VALUES ('cultivo_tipo', NULL, NULL, '0', NULL, NULL);
 INSERT INTO domains.cultivo_tipo (category, key, value, ordering, parent, tooltip) VALUES ('cultivo_tipo', 'Arroz', NULL, NULL, NULL, NULL);
@@ -145,8 +145,8 @@ INSERT INTO domains.licencia_tipo (category, key, value, ordering, parent, toolt
 INSERT INTO domains.licencia_tipo (category, key, value, ordering, parent, tooltip) VALUES ('licencia_tipo', 'Superficial', NULL, NULL, NULL, NULL);
 
 INSERT INTO domains.pagamentos (category, key, value, ordering, parent, tooltip) VALUES ('pagamentos', NULL, NULL, '0', NULL, NULL);
-INSERT INTO domains.pagamentos (category, key, value, ordering, parent, tooltip) VALUES ('pagamentos', 'Non pagada', NULL, NULL, NULL, NULL);
-INSERT INTO domains.pagamentos (category, key, value, ordering, parent, tooltip) VALUES ('pagamentos', 'Pagada', NULL, NULL, NULL, NULL);
+INSERT INTO domains.pagamentos (category, key, value, ordering, parent, tooltip) VALUES ('pagamentos', 'Non pagada', 'false', NULL, NULL, NULL);
+INSERT INTO domains.pagamentos (category, key, value, ordering, parent, tooltip) VALUES ('pagamentos', 'Pagada', 'true', NULL, NULL, NULL);
 
 
 INSERT INTO domains.posto (category, key, value, ordering, parent, tooltip) VALUES ('posto', NULL, NULL, '0', 'Ancuabe', NULL);
