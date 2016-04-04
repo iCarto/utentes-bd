f4838b83f564f37a5c4f9da745e39df02819dd42
Francisco Puga <fpuga@icarto.es>
2016-04-04 13:23:09 +0200

    Revert 'drop constraints for testing'

--- deploy/drop_constraints_for_testing.sql
+++
@@ -1,7 +0,0 @@
--- Deploy utentes:drop_constraints_for_testing to pg
-
-BEGIN;
-
-alter table utentes.actividades_cultivos ALTER COLUMN the_geom DROP NOT NULL;
-
-COMMIT;
--- revert/drop_constraints_for_testing.sql
+++
@@ -1,7 +0,0 @@
--- Revert utentes:drop_constraints_for_testing from pg
-
-BEGIN;
-
-
-alter table utentes.actividades_cultivos ALTER COLUMN the_geom SET NOT NULL;
-COMMIT;
