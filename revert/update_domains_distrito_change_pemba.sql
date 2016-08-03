aa8d8a6a4988032615969091785be5d0b8701127
Francisco Puga <fpuga@icarto.es>
2016-08-03 16:51:57 +0200

    Changes a value in the domains distrito table

---
+++ revert/update_domains_distrito_change_pemba.sql
@@ -0,0 +1,13 @@
+-- Revert utentes:update_domains_distrito_change_pemba from pg
+
+BEGIN;
+
+UPDATE domains.distrito SET key='Pemba' WHERE (category, key, parent) = ('distrito', 'Pemba - Metuge', 'Cabo Delgado');
+
+UPDATE domains.posto SET parent='Pemba' WHERE (category, ordering, parent) = ('posto', 0, 'Pemba - Metuge');
+UPDATE domains.posto SET parent='Pemba' WHERE (category, key, parent) = ('posto', 'Metuge', 'Pemba - Metuge');
+UPDATE domains.posto SET parent='Pemba' WHERE (category, key, parent) = ('posto', 'Mieze', 'Pemba - Metuge');
+
+ALTER TABLE domains.posto DROP CONSTRAINT posto_parent_fkey;
+
+COMMIT;
