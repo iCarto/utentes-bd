bee02ccee80f6fac1da74845e9caecb71fb3ad94
Francisco Puga <fpuga@icarto.es>
2016-08-04 09:23:09 +0200

    Introduces the database as parameter in the scripts

--- scripts/dump_bd.sh
+++ scripts/dump_bd.sh
@@ -1,7 +1,9 @@
 
 filename=utentes-ara-norte-`date +%Y%m%d`.sql
+DATABASE=sixhiara
 
+echo "CREATE IF NOT EXISTS postgis;" > $filename
 echo "DROP SCHEMA IF EXISTS utentes CASCADE;" > $filename
 echo "DROP SCHEMA IF EXISTS domains CASCADE;" >> $filename
 
-pg_dump -h localhost -p 5432 -U postgres --column-inserts --schema utentes --schema domains sixhiara >> $filename
+pg_dump -h localhost -p 5432 -U postgres --column-inserts --schema utentes --schema domains $DATABASE >> $filename
--- scripts/subir_datos.sh
+++ scripts/subir_datos.sh
@@ -1,5 +1,6 @@
 # usage subir_datos.sql PATH_AL_SHP
+DATABASE=sixhiara
 
-shp2pgsql -d -s 32737 -W "cp1250" $1 public.entrada  | psql -h localhost -U postgres -d sixhiara
-psql -h localhost -U postgres -d sixhiara -f subir_datos.sql
-psql -h localhost -U postgres -d sixhiara -c "DROP TABLE public.entrada;"
+shp2pgsql -d -s 32737 -W "cp1250" $1 public.entrada  | psql -h localhost -U postgres -d $DATABASE
+psql -h localhost -U postgres -d $DATABASE -f subir_datos.sql
+psql -h localhost -U postgres -d $DATABASE -c "DROP TABLE public.entrada;"
