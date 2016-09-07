9fe1ca774e09d2e0e1cd06bed80ec4b85d3cf175
Francisco Puga <fpuga@icarto.es>
2016-09-07 20:05:50 +0200

    New script to insert the pictures in the database from a backup

---
+++ scripts/restore_pictures_from_backup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DATABASE=sixhiara
+
+TODAY=`date +%Y%m%d`
+
+BACKUP_FILE="${1}"
+
+if [ ! -f $BACKUP_FILE ] ; then
+    echo "El fichero de backup debe existir"
+    exit
+fi
+
+
+PSQL="psql -X -q -v ON_ERROR_STOP=1 --pset pager=off"
+
+PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABASE} -c "DELETE FROM inventario.acuiferos_imagenes; DELETE FROM inventario.barragens_imagenes; DELETE FROM inventario.estacoes_imagenes; DELETE FROM inventario.fontes_imagenes;"
+
+pg_restore -U postgres -h localhost -d ${DATABASE} -a -t acuiferos_imagenes ${BACKUP_FILE}
+pg_restore -U postgres -h localhost -d ${DATABASE} -a -t barragens_imagenes ${BACKUP_FILE}
+pg_restore -U postgres -h localhost -d ${DATABASE} -a -t estacoes_imagenes ${BACKUP_FILE}
+pg_restore -U postgres -h localhost -d ${DATABASE} -a -t fontes_imagenes ${BACKUP_FILE}
