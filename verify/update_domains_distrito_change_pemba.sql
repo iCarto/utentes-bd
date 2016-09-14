df08d8ba83b9ffcb2038b65073a029fbf8bc5ba4
Francisco Puga <fpuga@icarto.es>
2016-09-14 20:00:17 +0200

    Drops a change. It is directly made in the original patches

--- verify/update_domains_distrito_change_pemba.sql
+++
@@ -1,8 +0,0 @@
--- Verify utentes:update_domains_distrito_change_pemba on pg
-
-BEGIN;
-
-SELECT 1/count(*) FROM domains.distrito
-WHERE (category, key, parent) = ('distrito', 'Pemba - Metuge', 'Cabo Delgado');
-
-ROLLBACK;
