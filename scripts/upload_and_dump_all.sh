527bc836a451feba85619392303741eac9cd652d
Francisco Puga <fpuga@icarto.es>
2017-08-02 20:39:01 +0200

    Deployment script includes dpmaip database

--- scripts/upload_and_dump_all.sh
+++ scripts/upload_and_dump_all.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# DATABASE = $1
+# DATA_VERSION = $2
+SQITCH_TAG='@20170503'
+
+
+SUCCESS=0
 TODAY=`date +%y%m%d`
 PSQL="psql -X -q -v ON_ERROR_STOP=1 --pset pager=off"
 PGDUMP="/usr/lib/postgresql/9.5/bin/pg_dump"
 
-# DATABASE = $1
-# DATA_VERSION = $2
+
 foo() {
     PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${1} -f cbase.sql.$2
     PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${1} -f acuiferos.sql.$2
@@ -20,13 +25,28 @@ foo2() {
     PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${1} -f ./datos/170421_${1}.sql
 }
 
+# $1 = DATABASE
+# $2 = TAG OR @HEAD
+sqitch_deploy() {
+    cd ..
+    sqitch deploy db:pg://postgres:postgres@localhost:5432/$1 --to-change $2
+    if ! [ "$?" -eq $SUCCESS ] ; then
+	echo 'Sqitch no ha finalizado correctamente'
+	echo "$1, $2"
+	echo "Saliendo"
+	exit
+    fi
+    cd scripts
+}
 
 
+main() {
 ${PSQL} -h localhost -U postgres -d postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where datname IN ('aranorte', 'aranorte_test', 'arasul');"
 dropdb -h localhost -U postgres arasul
 dropdb -h localhost -U postgres aranorte
 dropdb -h localhost -U postgres aranorte_test
 dropdb -h localhost -U postgres vacia
+dropdb -h localhost -U postgres dpmaip
 dropuser -h localhost -U postgres utentes
 dropuser -h localhost -U postgres sixhiara_owner
 dropuser -h localhost -U postgres elle_read
@@ -38,33 +58,55 @@ dropuser -h localhost -U postgres inventario_write
 dropuser -h localhost -U postgres inventario
 
 DATABASE=vacia
+echo -e "\n\n\nWORKING IN ${DATABASE}\n\n\n"
 createdb -h localhost -U postgres -T template0 -E UTF8 ${DATABASE}
-cd ..
-sqitch deploy db:pg://postgres:postgres@localhost:5432/vacia
-cd scripts
+sqitch_deploy $DATABASE $SQITCH_TAG
 
 
 DATABASE=aranorte
+echo -e "\n\n\nWORKING IN ${DATABASE}\n\n\n"
 NORTE_DATA_VERSION=20160916.Norte
 createdb -h localhost -U postgres -T vacia ${DATABASE}
-foo ${DATABASE} ${NORTE_DATA_VERSION}
-foo2 ${DATABASE} ${NORTE_DATA_VERSION}
-bash restore_pictures_from_backup.sh fotos_inventario_20160918.Norte.backup ${DATABASE}
-${PGDUMP} -h localhost -U postgres -C -E UTF-8 -f /tmp/${TODAY}_BDD_${DATABASE}.backup -Fc -O -x -Z 9 ${DATABASE}
+PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABASE} -f cbase.sql.${NORTE_DATA_VERSION}
+# foo ${DATABASE} ${NORTE_DATA_VERSION}
+# PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABSE} -c "DO $$ DECLARE query text; BEGIN FOR query IN SELECT 'DELETE FROM ' || schemaname || '.' || tablename  from pg_tables where schemaname = 'inventario' LOOP EXECUTE query; END LOOP; END $$;"
 
+# En lugar de disable-triggers estaria bien poder hacer un SET ALL CONSTRAINTS DEFERRED, para dar más seguridad a que
+# la migración va como debe. El disable triggers es porque si hay claves foráneas pg_restore no lo gestiona y puede intentar
+# introducir datos de analise antes que datos de fuente y se produce un error
+# https://stackoverflow.com/questions/5359968/restore-postgresql-db-from-backup-without-foreign-key-constraint-issue
+# http://blog.fabianbecker.eu/pg_restore-and-foreign-key-constraints/
+pg_restore --disable-triggers -a --single-transaction -h localhost -U postgres -d ${DATABASE} ./datos/170421_backupinventario.backup
+foo2 ${DATABASE} ${NORTE_DATA_VERSION}
+# bash restore_pictures_from_backup.sh fotos_inventario_20160918.Norte.backup ${DATABASE}
+
+sqitch_deploy $DATABASE @HEAD
+[ $FLAG_DUMP -eq 1 ] && ${PGDUMP} -h localhost -U postgres -C -E UTF-8 -f /tmp/${TODAY}_BDD_${DATABASE}.backup -Fc -O -x -Z 9 ${DATABASE}
 
 
 
 DATABASE=aranorte_test
+echo -e "\n\n\nWORKING IN ${DATABASE}\n\n\n"
 createdb -h localhost -U postgres -T aranorte ${DATABASE}
 foo2 ${DATABASE} ${NORTE_DATA_VERSION}
+# sqitch_deploy $DATABASE @HEAD
 ${PGDUMP} -h localhost -U postgres -C -E UTF-8 -f /tmp/${TODAY}_BDD_${DATABASE}.backup -Fc -O -x -Z 9 ${DATABASE}
 
 
+DATABASE=dpmaip
+echo -e "\n\n\nWORKING IN ${DATABASE}\n\n\n"
+createdb -h localhost -U postgres -T vacia ${DATABASE}
+sqitch_deploy $DATABASE @HEAD
+# foo2 ${DATABASE} ${NORTE_DATA_VERSION}
+PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABASE} -c "DELETE FROM utentes.utentes; DELETE FROM utentes.settings;"
+PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABASE} -f ./datos/170421_aranorte_test.sql
+PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABASE} -c "DELETE FROM domains.ara; INSERT INTO domains.ara VALUES ('ara', 'DPMAIP', 'DPMAIP', NULL, NULL, NULL); REFRESH MATERIALIZED VIEW domains.domains;"
+[ $FLAG_DUMP -eq 1 ] && ${PGDUMP} -h localhost -U postgres -C -E UTF-8 -f /tmp/${TODAY}_BDD_${DATABASE}.backup -Fc -O -x -Z 9 ${DATABASE}
 
 
 
 DATABASE=arasul
+echo -e "\n\n\nWORKING IN ${DATABASE}\n\n\n"
 SUR_DATA_VERSION=20170417.Sul
 createdb -h localhost -U postgres -T vacia ${DATABASE}
 
@@ -74,7 +116,24 @@ PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DA
 PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABASE} -f ./sixhiara_ARA_Sul_Mapa.sql
 bash restore_pictures_from_backup.sh fotos_inventario_20170417.Sul.backup ${DATABASE}
 
+sqitch_deploy $DATABASE @HEAD
+PGOPTIONS='--client-min-messages=warning' $PSQL -h localhost -U postgres -d ${DATABASE} -c "DELETE FROM domains.ara; INSERT INTO domains.ara VALUES ('ara', 'Sul', 'Sul', NULL, NULL, NULL); REFRESH MATERIALIZED VIEW domains.domains;"
 
-${PGDUMP} -h localhost -U postgres -C -E UTF-8 -f /tmp/${TODAY}_BDD_${DATABASE}.backup -Fc -O -x -Z 9 ${DATABASE}
+[ $FLAG_DUMP -eq 1 ] && ${PGDUMP} -h localhost -U postgres -C -E UTF-8 -f /tmp/${TODAY}_BDD_${DATABASE}.backup -Fc -O -x -Z 9 ${DATABASE}
+
+}
 
 
+FLAG_DUMP=0
+# Process input arguments
+while [ $# -gt 0 ] ; do
+    case $1
+        in
+        --dump)
+            FLAG_DUMP=1
+	    shift
+    esac
+done
+
+
+main
