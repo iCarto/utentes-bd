967059a989299e67f233b7050e7144c8f483ec01
Pablo Sanxiao <psanxiao@icarto.es>
2018-04-11 16:58:43 +0200

    Added bacia and subacia fkey constraints to fontes table. Refs #1233

--- deploy/alter_table_fontes_add_columns_bacia_font_subacia_font.sql
+++ deploy/alter_table_fontes_add_columns_bacia_font_subacia_font.sql
@@ -5,4 +5,14 @@ BEGIN;
 ALTER TABLE inventario.fontes ADD COLUMN bacia character varying(254);
 ALTER TABLE inventario.fontes ADD COLUMN subacia text;
 
+ALTER TABLE inventario.fontes
+   ADD CONSTRAINT fontes_bacia_fkey
+   FOREIGN KEY (bacia)
+   REFERENCES inventario_dominios.bacia(key) ON UPDATE CASCADE;
+
+ALTER TABLE inventario.fontes
+    ADD CONSTRAINT fontes_subacia_fkey
+    FOREIGN KEY (bacia, subacia) 
+    REFERENCES inventario_dominios.subacia(parent, key) ON UPDATE CASCADE;
+
 COMMIT;
