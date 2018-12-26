51bc220c62fcd8d21dabcd921ada94460a6718b7
Francisco Puga <fpuga@icarto.es>
2018-12-26 12:25:51 +0100

    Correctly sets exploracao.cadastro_uni from licencia.cadastro
    refs #1545

--- deploy/alter_table_exploracaos_add_cadastro_uni.sql
+++ deploy/alter_table_exploracaos_add_cadastro_uni.sql
@@ -3,7 +3,9 @@
 BEGIN;
 
 ALTER TABLE utentes.exploracaos ADD COLUMN cadastro_uni TEXT;
-UPDATE utentes.exploracaos e SET cadastro_uni = (SELECT l.cadastro FROM utentes.licencias l WHERE e.gid = l.gid);
+UPDATE utentes.exploracaos e SET cadastro_uni = (SELECT l.cadastro FROM utentes.licencias l WHERE e.gid = l.exploracao LIMIT 1);
 ALTER TABLE utentes.fontes ADD COLUMN cadastro TEXT;
+
+ALTER TABLE utentes.licencias DROP COLUMN cadastro;
 
 COMMIT;
--- revert/alter_table_exploracaos_add_cadastro_uni.sql
+++ revert/alter_table_exploracaos_add_cadastro_uni.sql
@@ -2,7 +2,10 @@
 
 BEGIN;
 
+ALTER TABLE utentes.licencias ADD COLUMN cadastro TEXT;
+UPDATE utentes.licencias l SET cadastro = (SELECT l.cadastro_uni FROM utentes.exploracaos e WHERE e.gid = l.exploracao LIMIT 1);
 ALTER TABLE utentes.exploracaos DROP COLUMN cadastro_uni;
 ALTER TABLE utentes.fontes DROP COLUMN cadastro;
+
 
 COMMIT;
