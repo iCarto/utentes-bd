ec8b43d4079de2384a3a3995047ddf4c8b3a6f99
Francisco Puga <fpuga@icarto.es>
2020-05-05 10:30:49 +0200

    Drops table utentes.inventario_fontes
    Don't needed anymore
    
    refs #2076

---
+++ deploy/drop_table_utentes_inventario_fontes.sql
@@ -0,0 +1,7 @@
+-- Deploy utentes:drop_table_utentes_inventario_fontes to pg
+
+BEGIN;
+
+DROP TABLE utentes.inventario_fontes;
+
+COMMIT;
---
+++ verify/drop_table_utentes_inventario_fontes.sql
@@ -0,0 +1,7 @@
+-- Verify utentes:drop_table_utentes_inventario_fontes on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
